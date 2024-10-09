f6f7e5988468e76f68d27c8b00e0d33d8f6a9356
HP <HP@Jeevesh>
2024-10-09 17:14:15 +0530

    second commit

--- src/main/java/com/test1/t2/Test2.java
+++ src/main/java/com/test1/t2/Test2.java
@@ -2,7 +2,8 @@ package com.test1.t2;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("Git Test 2");
+		System.out.println("Git testing 2");
+		System.out.println("test 2");
 	}
 
 }
